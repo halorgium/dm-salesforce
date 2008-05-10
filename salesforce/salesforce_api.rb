e55d42197728bf459e0beaf89303e2513ed01a2b
Yehuda Katz <wycats@gmail.com>
2008-05-09 23:16:46 -0700

    Initial commit of Salesforce adapter.

---
+++ salesforce/salesforce_api.rb
@@ -0,0 +1,39 @@
+gem "soap4r", ">= 1.5.8"
+require 'soap/wsdlDriver'
+require 'soap/header/simplehandler'
+
+module SalesforceAPI
+
+  class HeaderHandler < SOAP::Header::SimpleHandler
+    def initialize(tag, value) 
+      super(XSD::QName.new('urn:partner.soap.sforce.com', tag)) 
+      @tag = tag 
+      @value = value 
+    end 
+    def on_simple_outbound 
+      @value 
+    end 
+  end
+  
+  class Connection
+    @@pushed = false
+    
+    attr_accessor :driver
+    def initialize(username, password, drivers)
+      $:.push drivers unless @@pushed
+      @@pushed = true
+      require "SalesforceAPIDriver"
+      
+      @driver = SalesforceAPI::Soap.new
+      begin
+        result = driver.login(:username => username, :password => password).result
+      rescue SOAP::FaultError => e
+        return nil
+      end
+      driver.endpoint_url = result.serverUrl
+      driver.headerhandler << HeaderHandler.new("SessionHeader", "sessionId" => result.sessionId)
+      driver.headerhandler << HeaderHandler.new("CallOptions", "client" => "client")
+      @user_details = result.userInfo
+    end
+  end
+end
