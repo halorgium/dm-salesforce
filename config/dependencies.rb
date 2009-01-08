4bf949b7bfde633f84b53bfb23721e5c6686a3af
Corey Donohoe <atmos@atmos.org>
2009-01-08 10:50:39 -0700

    update to a sane data_objects version scheme

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -11,5 +11,5 @@ add_dependency "extlib", "~> 0.9.9"
 add_dependency "dm-core", "~> 0.9.8", :require => 'dm-core'
 add_dependency "dm-validations", "~> 0.9.8", :require => 'dm-validations'
 add_dependency "soap4r", "~> 1.5.8"
-add_dependency "data_objects", "0.9.9"
-add_dependency "do_sqlite3", "0.9.9"
+add_dependency "data_objects", "~> 0.9.9"
+add_dependency "do_sqlite3", "~> 0.9.9"
