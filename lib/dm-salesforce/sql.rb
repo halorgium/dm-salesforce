9e4b9ec0f3704889318bfec67711f4e64a3b7a73
Jordan Ritter <jpr5@darkridge.com>
2009-10-14 18:58:19 -0700

    Cherry-picked part of 1c38b8, eliminating deprecation warnings.

--- lib/dm-salesforce/sql.rb
+++ lib/dm-salesforce/sql.rb
@@ -2,6 +2,81 @@ module DataMapperSalesforce
   module SQL
     class << self
       def from_condition(condition, repository)
+        slug = condition.class.slug
+        condition = case condition
+                    when DataMapper::Query::Conditions::AbstractOperation then condition.operands.first
+                    when DataMapper::Query::Conditions::AbstractComparison
+                      if condition.subject.kind_of?(DataMapper::Associations::Relationship)
+                        foreign_key_conditions(condition)
+                      else
+                        condition
+                      end
+                    else raise("Unkown condition type #{condition.class}: #{condition.inspect}")
+                    end
+
+        value = condition.value
+        prop = condition.subject
+        operator = case slug
+                   when String then operator
+                   when :eql, :in then equality_operator(value)
+                   when :not      then inequality_operator(value)
+                   when :like     then "LIKE #{quote_value(value)}"
+                   when :gt       then "> #{quote_value(value)}"
+                   when :gte      then ">= #{quote_value(value)}"
+                   when :lt       then "< #{quote_value(value)}"
+                   when :lte      then "<= #{quote_value(value)}"
+                   else raise "CAN HAS CRASH?"
+                   end
+        case prop
+        when DataMapper::Property
+          "#{prop.field} #{operator}"
+        when DataMapper::Query::Path
+          rels = prop.relationships
+          names = rels.map {|r| storage_name(r, repository) }.join(".")
+          "#{names}.#{prop.field} #{operator}"
+        end
+      end
+
+      def storage_name(rel, repository)
+        rel.parent_model.storage_name(repository.name)
+      end
+
+      def order(direction)
+        "#{direction.target.field} #{direction.operator.to_s.upcase}"
+      end
+
+      def equality_operator(value)
+        case value
+        when Array then "IN #{quote_value(value)}"
+        else "= #{quote_value(value)}"
+        end
+      end
+
+      def inequality_operator(value)
+        case value
+        when Array then "NOT IN #{quote_value(value)}"
+        else "!= #{quote_value(value)}"
+        end
+      end
+
+      def quote_value(value)
+        case value
+        when Array then "(#{value.map {|v| quote_value(v)}.join(", ")})"
+        when NilClass then "NULL"
+        when String then "'#{value.gsub(/'/, "\\'").gsub(/\\/, %{\\\\})}'"
+        else "#{value}"
+        end
+      end
+    end
+  end
+end
+
+
+
+
+=begin
+    class << self
+      def from_condition(condition, repository)
         op, prop, value = condition
         operator = case op
           when String then operator
@@ -56,5 +131,4 @@ module DataMapperSalesforce
         end
       end
     end
-  end
-end
+=end
